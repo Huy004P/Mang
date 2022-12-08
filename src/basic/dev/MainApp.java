4f0441028aa8b242acc36882eeb617aa4c2a52ad
Huy004P <114895245+Huy004P@users.noreply.github.com>
2022-12-08 13:55:49 +0700

    Add files via upload

--- src/basic/dev/MainApp.java
+++ src/basic/dev/MainApp.java
@@ -1,67 +1,73 @@
 package basic.dev;
 
-import java.util.Scanner;
+import java.util.Random;
 
-public class MainApp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Viết chương trình nhập vào một mảng 10 phần tử số nguyên
-		Scanner sc = new Scanner(System.in);
-		int[] arr = new int [10];
-		int tong  =0;
-		for (int i = 0; i < arr.length; i++) {
-		System.out.format("nhập vào arr [%d]", i);
-		arr[i] = sc.nextInt();
-		tong += arr [i];
+	public class MainApp {
+		public static void main(String[] args) {
+	int a[] = new int[10];
+	input(a);
+	print(a);
+    demChanLe(a);
+	sapXep(a);
+	demSoNguyenTo(a);
 	}
-		//Tính tổng các phần tửcủa mảng. Kiểm tra tổng làchẵn hay lẻ.
-		//In kết quảra màn hình
-		System.out.println("Tong cac phan tu arr[i] = " + tong);
-		if (tong %2 == 0) {
-			System.out.println("=> la so chan");
-		}else {
-			System.out.println("=> la so le");
+		private static void demSoNguyenTo(int[] a) {
+			//
+			int dem = 0;
+			for (int i = 0 ; i<a.length; i++) {
+				if(isPrimary(a[i]))
+					dem++;
+			}
+			System.out.format("Mang co %d so nguyen to", dem).println();
 		}
-		//Kiểm tra mảng cóbao nhiêu sốchẵn, bao nhiêu sốlẻ. In kết quảra màn hình
-		int ketQua = 0;
-		int ketQua2 =0;
-		for (int i=0; i < arr.length; i++) {
-			if (arr [i] %2 == 0);
-			ketQua++;
-			if (arr [i] %2 != 0);
-		}
-		System.out.format("co %d so chan", ketQua);
-		System.out.format(" va %d so le", ketQua2);
-	
-		//Sắp xếp các phần tửmảng theo thứtựtăng dần, giảm dần. In mảng ra màn hình.
-		//Tăng dần
-	        for (int i = 0 ; i < arr.length - 1; i++) {
-	            for (int j = i + 1; j < arr.length; j++) {
-	                if (arr[i] > arr[j]) {
-	                	int tg = arr[i];
-	                	arr[i] = arr[j];
-	                	arr[j] = tg;
-	      }
-	 }	
-}
-	        for (int j = 0; j < arr.length; j++) {
-	        	System.out.print(arr[j] + " ");
-	        }
-	        System.out.println();
-	        //Giảm dần
-	        int tg = arr[0];
-	        for (int i = 0; i < arr.length - 1; i++) {
-				for (int j = i + 1; j < arr.length; j++) {
-					if (arr[i] < arr[j]) {
-						tg = arr[j];
-						arr[j] = arr[i];
-						arr[i] = tg;
+		private static void sapXep(int[] a) {
+			//
+			for (int i = 0; i < a.length -1; i++) {
+				for(int j = i + 1; j < a.length; j++ ) {
+					if(a[i] > a[j]) {
+						int temp = a [i];
+						a[i] = a[j];
+						a[j] = temp;
 					}
 				}
 			}
-			for (int i = 0; i < arr.length; i++) {
-				System.out.print(arr[i] + " ");
-	        }
+		}
+		private static void demChanLe(int a[]) {
+			//
+	  int demChan = 0;
+	  for (int i = 0; i < a.length; i++) {
+		  if(a[i] % 2 == 0)
+			  demChan++;
+	  }
+	  System.out.format("%d so chan, %d so le", demChan, a.length - demChan);
 	}
+		private static void print(int[] a) {
+			//
+			System.out.println("Mang a");
+			for (int i = 0; i < a.length; i++) {
+				System.out.format("%d", a[i]).print("\t");
+			}
+			System.out.println();
+		}
+		private static void input(int[] a) {
+			//
+			int min = 1;
+			int max = 100;
+			for (int i = 0; i < a.length; i++) {
+				a[i] = randomInteger(min, max);
+			}
+		}
+		private static int randomInteger(int min, int max) {
+			//
+			Random rand = new Random();
+			return rand.nextInt(max + 1 - min) + min;
+		}
+		static boolean isPrimary(int n) {
+			if(n == 1 || n == 2) return true;
+			for (int i = 2; i < n; i++) {
+				if(n % i == 0) return false;
+			}
+			return true;
+		}
+
 }
